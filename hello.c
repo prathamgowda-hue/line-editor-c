a8d4307878b80769679049b16a4f1efc286bc17a
prathamgowdahue <prathamvggowda@gmail.com>
2026-08-31 11:32:47 +0530

    Update hello world greeting

--- hello.c
+++ hello.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void greet(const char *name);   // Function declaration
+
 int main()
 {
+    greet("Ada");
+
     printf("Hello World!\n");
+
     return 0;
+}
+
+void greet(const char *name)
+{
+    printf("Hello, %s! Welcome to your GitHub portfolio.\n", name);
 }
